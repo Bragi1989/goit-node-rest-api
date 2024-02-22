b8633af5a54460ec15a91f843541d277aea4d0d9
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2024-02-22 23:11:48 +0100

    123

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const mongoose = require("mongoose");
 
+const DB_URI = process.env.DB_URI;
 
 mongoose
-  .connect(process.env.DB_URI)
+  .connect(DB_URI)
   .then(() => console.log("Database connection successfully"))
   .catch((error) => {
     console.error("Database connection error:", error);
